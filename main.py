4d28d325ce34c337be5c436ff7aea5a603127821
Youssef Moussa <youssefmoussaPC92@gmail.com>
2026-06-12 20:54:36 +0300

    Updating main file

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-from src.load_data      import load_data
-from src.clean_basic    import clean_basic
-from src.clean_victims  import clean_victims
-from src.clean_location import clean_location
-from src.export         import export_data
+from src.data_cleaning.load_data      import load_data
+from src.data_cleaning.clean_basic    import clean_basic
+from src.data_cleaning.clean_victims  import clean_victims
+from src.data_cleaning.clean_location import clean_location
+from src.data_cleaning.export         import export_data
+
+from src.feature_engineering.time_features    import add_time_features
+from src.feature_engineering.spatial_features import add_spatial_features
+from src.feature_engineering.victim_features  import add_victim_features
+from src.feature_engineering.crime_features   import add_crime_features
+from src.feature_engineering.premise_features import add_premise_features
 
 load_dotenv()
 
 DATA_PATH   = os.getenv("DATA_PATH")
 INPUT_FILE  = Path(DATA_PATH) / "Crime_Data_from_2020_to_2024.csv" # type: ignore
-OUTPUT_FILE = Path("data/processed/cleaned_crime_data.parquet")
+CLEAN_FILE  = Path("data/processed/cleaned_crime_data.parquet")
+FE_FILE     = Path("data/processed/feature_engineered_crime_data.parquet")
 
 
 def run_pipeline():
@@ -28,8 +35,18 @@ def run_pipeline():
     print("\n── Clean Location ────────────────────────────────")
     df = clean_location(df)
 
-    print("\n── Export ────────────────────────────────────────")
-    export_data(df, OUTPUT_FILE)
+    print("\n── Export Cleaned ────────────────────────────────")
+    export_data(df, CLEAN_FILE)
+
+    print("\n── Feature Engineering ───────────────────────────")
+    df = add_time_features(df)
+    df = add_spatial_features(df)
+    df = add_victim_features(df)
+    df = add_crime_features(df)
+    df = add_premise_features(df)
+
+    print("\n── Export Feature Engineered ─────────────────────")
+    export_data(df, FE_FILE)
 
     print("\nPipeline complete.")
 
